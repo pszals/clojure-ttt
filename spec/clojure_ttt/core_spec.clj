a046a55c96b207b43891a2eef263b9c32e670e3a
Philip Szalwinski <pszalwinski@gmail.com>
2013-10-29 11:26:56 -0500

    Can place marker on board; check for winners across; working on vertical winners

--- spec/clojure_ttt/core_spec.clj
+++ spec/clojure_ttt/core_spec.clj
@@ -2,19 +2,38 @@
   (:require [speclj.core :refer :all]
             [clojure-ttt.core :refer :all]))
 
-(describe "Truth"
-  (it "is true"
-    (should true))
-  
-  (it "is not false"
-    (should-not false)))
-
-(describe "Addition of two numbers"
-  (it "Adds 2 and 2"
-    (should= 4 (add-two 2 2))))
-
 (describe "Placing a piece on the board"
   (it "Modifies board for a given square"
-    (should= '("x" 2 3 4 5 6 7 8 9) (place-piece 1 "x" '(1 2 3 4 5 6 7 8 9)))))
+    (should= ["x" 2 3 4 5 6 7 8 9] (place-piece 1 "x" [1 2 3 4 5 6 7 8 9]))))
+
+(describe "Validating a move"
+  (it "Checks if square is occupied"
+    (should= false (open? ["x"] 1)))
+
+  (it "Checks if square is not occupied"
+    (should= true (open? [1] 1))))
+
+(describe "Finding a winner"
+  (it "Checks first row for three of a kind"
+    (should= false (row-winner? [1 2 3 4 5 6 7 8 9])))
+  
+  (it "Returns true if three elements are all the same"
+    (should= true (three-of-a-kind? ["x" "x" "x"])))
+
+  (it "Returns false if three elements are not all the same"
+    (should= false (three-of-a-kind? ["x" "x" 3])))
+
+  (it "Checks first row for three of a kind"
+    (should= true (row-winner? ["x" "x" "x" 4 5 6 7 8 9]))) 
+
+  (it "Checks second row for three of a kind"
+    (should= true (row-winner? [1 2 3 "x" "x" "x" 7 8 9]))))
+
+(describe "Gathering winning combinations"
+  (it "Gets rows from board"
+    (should= [[1 2 3] [4 5 6] [7 8 9]] (rows [1 2 3 4 5 6 7 8 9])))
+  
+  (it "Gets columns from board"
+    (should= [[1 4 7] [2 5 8] [3 6 9]] (columns [1 2 3 4 5 6 7 8 9]))))
 
 (run-specs)
--- src/clojure_ttt/core.clj
+++ src/clojure_ttt/core.clj
@@ -1,6 +1,20 @@
 (ns clojure-ttt.core)
-  (defn add-two [a b]
-    (+ a b))
 
-  (defn place-piece [square marker board])
+  (defn place-piece [square marker board]
+    (assoc board (- square 1) marker))   
+ 
+  (defn open? [board square]
+    (number? (nth board (- square 1))))
 
+  (defn rows [board]
+    (partition 3 board))
+  
+  (defn three-of-a-kind? [row]
+    (= (nth row 0) (nth row 1) (nth row 2)))
+  
+  (defn row-winner? [board]
+    (let [across (rows board)]
+      (or (three-of-a-kind? (nth across 0)) (three-of-a-kind? (nth across 1)) (three-of-a-kind? (nth across 2)))))
+
+  (defn columns [board]
+    [[1 4 7] [2 5 8] [3 6 9]])
