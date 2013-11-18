4dcda2b8294dd2197861875fca332bd676e01c27
Philip Szalwinski <pszalwinski@gmail.com>
2013-11-17 18:29:15 -0600

    Added high level test to runner

--- spec/clojure_ttt/runner_spec.clj
+++ spec/clojure_ttt/runner_spec.clj
@@ -1,6 +1,16 @@
 (ns clojure-ttt.runner_spec
   (:require [speclj.core :refer :all]
             [clojure-ttt.runner :refer :all]
+            [clojure-ttt.players :refer :all]
             [clojure-ttt.input-output :refer :all]))
 
+(describe "Game Cycle"
+  (it "Finishes the game when there is a move left to win"
+    (with-redefs [println (fn [& _] "Player O Wins")
+                  take-turn (fn [& _] "2")]
+      (should= "Player O Wins" (play-game 
+                         ["x"  2  "x"
+                          "o" "o"  6
+                          "x" "o" "o"])))))
+      
     
